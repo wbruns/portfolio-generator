f746a469470779f828bf3d839ceea056da10120e
wbruns <wbruns07@gmail.com>
2022-02-15 17:55:10 -0600

    app.js now writes a html file

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// assignment destructing
+const [name, github] = profileDataArgs;
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('=====================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+// const printProfileData = profileDataArr => {
+//     for (let i=0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('=====================');
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
 };
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
